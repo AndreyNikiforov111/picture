7fc946c1aedc9058cb43bec868a798b664c09bc1
AndreyNikiforov111 <andrey.nikiforov.116@gmail.com>
2024-03-31 17:13:10 +0600

    second

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base:'./',
+    //publicDir: '/public/',
     plugins: [
         laravel({
+            //assetsDir: 'public/build', // Set the assetsDir to reflect the public directory
+
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
